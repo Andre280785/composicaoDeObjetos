94a423298e803fc4e96690b09302802837890392
André Tavares Machado <andre266@gmail.com>
2020-02-18 22:39:41 -0300

    Exercicio Composição de Objetos

---
+++ ExercicioComposicao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ExercicioComposicao.Entities;
+using ExercicioComposicao.Entities.Enums;
+using System.Globalization;
+
+namespace ExercicioComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            DateTime moment = DateTime.Now;
+            Order order = new Order(moment, status, client);
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string product = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product prod = new Product(product, price);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                OrderItem item = new OrderItem(quantity, price, prod);
+                order.AddItem(item);
+            } 
+
+            
+        }
+    }
+}
